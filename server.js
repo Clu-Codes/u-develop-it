0d82de4216676831bdccfeb1bf10ee9320ac599b
Clu <retsulcconnor@gmail.com>
2021-01-17 11:32:11 -0500

    added post route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inputCheck = require('./utils/inputCheck');
 // verbose produces messages in the terminal regarding the state of the runtime to help explain what the application is doing. 
 const sqlite3 = require('sqlite3').verbose();
 const PORT = process.env.PORT || 3001;
@@ -23,31 +24,29 @@ app.get('/', (req, res) => {
 });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-// ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-    if (err) {
-        console.log(err);
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        return res.status(400).json({ error: errors });
     }
-    console.log(result, this.lastID);
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                VALUES(?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            return res.status(400).json({ err: err.message });
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            // lastID is referencing the last ID autogenerate by SQL and adding onto that
+            id: this.lastID
+        });
+    });
 });
 
 // Delete a candidate
-// app.delete('api/candidate/:id', (req, res) => {
-//     const sql = `DELETE FROM candidates WHERE id = ?`;
-//     const params = [req.params.id];
-//     db.run(sql, params, function(err, result) {
-//         if (err) {
-//             return res.status(400).json({ error: res.message });
-//         }
-//         res.json({
-//             message: 'successfully deleted',
-//             changes: this.changes
-//         });
-//     });
-// });
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
